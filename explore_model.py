8d7ee35fb2f8f251129cb5b1c38360b5039fe633
Thai Thien <ttpro1995@yahoo.com.vn>
2020-11-23 12:39:59 +0700

    Auto stash before merge of "dev" and "origin/dev"

--- explore_model.py
+++ explore_model.py
@@ -1,11 +1,12 @@
 from model import ContextualModule
+from model import CANNet/
 
 def explore_contexture_module():
     contextual_module = ContextualModule(512, 512)
     # TODO: what is scales
     print(contextual_module.scales)
 
-
+def explort_CANNet():
 
 if __name__ == "__main__":
     explore_contexture_module()
